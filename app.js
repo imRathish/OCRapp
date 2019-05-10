83ce5a9ba6c5fa3934aec265778df3a852547c7e
S S, Subramanian <Subramanian.Ss@sc.com>
2019-05-10 09:50:09 +0530

    change

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express()
 const port=process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get('ocr',() => res.send('OCR'));
+app.get('/ocr',(req,res) => res.send('OCR'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
